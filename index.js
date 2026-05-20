aac65034218da5ac3b5de2cc87fedbeae8fc1ab5
negocios87-sketch <negocios87@boardacademy.com.br>
2026-05-20 14:31:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const path    = require('path');
 const app  = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'public')));
 
 const API_TOKEN       = process.env.PIPEDRIVE_TOKEN;
 const ORG             = process.env.PIPEDRIVE_ORG   || 'boardacademy';
